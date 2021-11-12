2d5e80a5bd5b20f72bb3eaa9849140b5ccba5179
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2021-11-12 11:46:37 +0700

    chinh sua bat ta modal

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,14 +12,24 @@ function download(){
 }
 //                                phan download CV 👆
 
-document.getElementById("files").onchange = function () {
-  var reader = new FileReader();
+// document.getElementById("files").onchange = function () {
+//   var reader = new FileReader();
 
-  reader.onload = function (e) {
-      // get loaded data and render thumbnail.
-      document.getElementById("image").src = e.target.result;
-  };
+//   reader.onload = function (e) {
+//       // get loaded data and render thumbnail.
+//       document.getElementById("image").src = e.target.result;
+//   };
 
-  // read the image file as a data URL.
-  reader.readAsDataURL(this.files[0]);
-};
+//   // read the image file as a data URL.
+//   reader.readAsDataURL(this.files[0]);
+// };
+
+document.getElementById("ava").onclick = function () {
+  document.getElementById("OCModal").classList.add("OpenModal");
+}
+document.getElementById("js-closeModalAva").onclick = function () {
+  document.getElementById("OCModal").classList.remove("OpenModal");
+}
+document.getElementById("opaModal").onclick = function () {
+  document.getElementById("OCModal").classList.remove("OpenModal");
+}
