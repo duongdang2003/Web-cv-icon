7415331bac3f98c1179541374e324666d14c3529
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2021-11-12 11:23:21 +0700

    add new

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,3 +12,14 @@ function download(){
 }
 //                                phan download CV 👆
 
+document.getElementById("files").onchange = function () {
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      document.getElementById("image").src = e.target.result;
+  };
+
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+};
