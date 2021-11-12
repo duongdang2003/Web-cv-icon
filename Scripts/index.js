37bbc20dc5f5b6d2ade53c40c062d923edc3f390
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2021-11-12 11:52:12 +0700

    add file

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,17 +12,17 @@ function download(){
 }
 //                                phan download CV 👆
 
-// document.getElementById("files").onchange = function () {
-//   var reader = new FileReader();
+document.getElementById("files").onchange = function () {
+  var reader = new FileReader();
 
-//   reader.onload = function (e) {
-//       // get loaded data and render thumbnail.
-//       document.getElementById("image").src = e.target.result;
-//   };
+  reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      document.getElementById("image").src = e.target.result;
+  };
 
-//   // read the image file as a data URL.
-//   reader.readAsDataURL(this.files[0]);
-// };
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+};
 
 document.getElementById("ava").onclick = function () {
   document.getElementById("OCModal").classList.add("OpenModal");
